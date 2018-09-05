df7dc07978ff07bb989059a758daf4cde4c13c5e
Mykola Podolian <nkpodolyan@gamil.com>
2018-09-05 10:26:57 +0300

    Add android project

---
+++ app/src/main/java/com/top50/redit/reddittop50/RedditsListActivity.kt
@@ -0,0 +1,12 @@
+package com.top50.redit.reddittop50
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RedditsListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reddits_list)
+    }
+}
